2495980dc3a06eeb223e9935a4775169e6b90ef5
Andrzej Kaminski <andrzej.kaminski0591@gmail.com>
2019-11-23 19:43:57 +0100

    Preliminary Spring MVC project

---
+++ src/main/java/pl/example/SnackTimeAppApplication.java
@@ -0,0 +1,12 @@
+package pl.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackTimeAppApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/example/order/ClientOrder.java
@@ -0,0 +1,30 @@
+package pl.example.order;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import pl.example.item.Item;
+
+@Component
+@SessionScope
+public class ClientOrder {
+
+    private Order order;
+
+    public ClientOrder() {
+        clear();
+    }
+
+    Order getOrder() {
+        return order;
+    }
+
+    void add(Item item) {
+        order.getItems().add(item);
+    }
+
+    void clear() {
+        order = new Order();
+        order.setStatus(OrderStatus.NEW);
+    }
+}
